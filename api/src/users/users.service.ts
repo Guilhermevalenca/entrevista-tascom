ce932d8c15159022b40b0f630be226b37adbc846
GuilhermeValenca <guilherm3vrp@gmail.com>
2025-07-04 10:04:48 -0300

    feat: starting projects

---
+++ api/src/users/users.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { User, Prisma } from '@prisma/client';
+
+@Injectable()
+export class UsersService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async create(data: Prisma.UserCreateInput): Promise<User | null> {
+        return this.prisma.user.create({
+            data: data,
+        });
+    }
+
+    async find(id: number): Promise<User | null> {
+        return this.prisma.user.findUnique({
+            where: {
+                id,
+            },
+            include: {
+                groups: {
+                    include: {
+                        group: true,
+                    },
+                },
+            },
+        });
+    }
+
+    async update(id: number, data: Prisma.UserUpdateInput): Promise<User | null> {
+        return this.prisma.user.update({
+            data,
+            where: {
+                id,
+            },
+        });
+    }
+
+    async delete(id: number): Promise<User | null> {
+        return this.prisma.user.delete({
+            where: {
+                id,
+            },
+        });
+    }
+
+    async findEmail(email: string): Promise<User | null> {
+        return this.prisma.user.findUnique({
+            where: {
+                email,
+            },
+        });
+    }
+}
